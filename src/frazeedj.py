60663f8ee86a80fe3350997ebf325ee2bc8920c3
frazeedj <frazeedj@rose-hulman.edu>
2018-11-06 13:27:51 -0500

    beacon buttons

--- src/frazeedj.py
+++ src/frazeedj.py
@@ -10,6 +10,15 @@ import time
 
 def main():
     """ Runs YOUR specific part of the project """
+    tests()
+
+
+def tests():
+    # test_touch_sensor()
+    test_drive_until_color(rb.Color.BLACK)
+
+
+def test_touch_sensor():
     print('Testing "wait_until_pressed" ')
     ts = rb.TouchSensor()
     if ts.wait_until_pressed():
@@ -19,7 +28,21 @@ def main():
     if ts.wait_until_released():
         print('Test Complete')
 
-    x = rb.InfraredAsProximitySensor.get_distance_to_nearest_object()
-    print(x)
+
+def test_drive_until_color(color):
+    drive = rb.DriveSystem()
+    while True:
+        drive.start_moving()
+        if rb.ColorSensor.get_color == color:
+            drive.stop_moving()
+            break
+
+
+def beep_if_detect():
+    while True:
+        for k in range(9, 16):
+            if rb.InfraredAsProximitySensor.get_distance_to_nearest_object_in_inches == k:
+                ev3.Sound.beep(1)
+
 
 main()
