e101e41de05258e84d10446543b8c7eb89cef67c
frazeedj <frazeedj@rose-hulman.edu>
2018-11-06 19:50:05 -0500

    tests in my file

--- src/frazeedj.py
+++ src/frazeedj.py
@@ -4,7 +4,9 @@
 """
 
 import rosebotics_new as rb
-import ev3dev.ev3 as ev3 #sounds
+import ev3dev.ev3 as ev3  # sounds
+import tkinter
+from tkinter import ttk
 import time
 
 
@@ -16,7 +18,8 @@ def main():
 def tests():
     # test_touch_sensor()
     # test_drive_until_color(rb.Color.BLACK)
-    test_beep_if_detect()
+    # test_beep_if_detect()
+    test_beacon_buttons()
 
 def test_touch_sensor():
     print('Testing "wait_until_pressed" ')
@@ -40,13 +43,24 @@ def test_drive_until_color(color):
 
 def test_beep_if_detect():
     while True:
-        for k in range(9, 16):
-            if rb.InfraredAsProximitySensor.get_distance_to_nearest_object_in_inches == k:
-                ev3.Sound.beep(1)
+        if rb.InfraredAsProximitySensor.get_distance_to_nearest_object_in_inches is True:
+            for k in range(9, 16):
+                if rb.InfraredAsProximitySensor.get_distance_to_nearest_object_in_inches == k:
+                    ev3.Sound.beep(1)
 
 
 def test_beacon_buttons():
+    root = tkinter.Tk()
+    frame1 = ttk.Frame(root, padding=10)
+    frame1.grid()
+    button1 = ttk.Button(frame1, text='Infrared Beacon Buttons')
+    button1.grid()
 
+    while True:
+        if rb.InfraredAsBeaconButtonSensor.is_top_red_button_pressed is True:
+            rb.DriveSystem.go_straight_inches(11)
+        if rb.InfraredAsBeaconButtonSensor.is_top_blue_button_pressed is True:
+            rb.DriveSystem.go_straight_inches(-11)
 
 
 
